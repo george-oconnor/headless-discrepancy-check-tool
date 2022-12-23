0523725e3354f61f59216a62ca61153c890c0784
George O Connor <george@georgestools.com>
2022-12-23 15:08:24 +0000

    added batch file

---
+++ discrepancy_check.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\filemakeradmin\Desktop\George\attendance-app"
+"C:\Users\filemakeradmin\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\filemakeradmin\Desktop\George\headless-discrepancy-check-tool\headless_discrepancy_check.py"

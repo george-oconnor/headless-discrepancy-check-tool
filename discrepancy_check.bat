eca2a683b26ae6bc8034adad50d973725b0155a3
George O Connor <george@georgestools.com>
2022-12-23 15:35:27 +0000

    typo

--- discrepancy_check.bat
+++ discrepancy_check.bat
@@ -1,2 +1,2 @@
-cd "C:\Users\filemakeradmin\Desktop\George\attendance-app"
-"C:\Users\filemakeradmin\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\filemakeradmin\Desktop\George\headless-discrepancy-check-tool\headless_discrepancy_check.py"
+cd "C:\Users\teamsattendanceadmin\OneDrive - The Institute of Education\Desktop\George\recording-schedule-app"
+"C:\Users\teamsattendanceadmin\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\teamsattendanceadmin\OneDrive - The Institute of Education\Desktop\George\headless-discrepancy-check-tool\headless_discrepancy_check.py"
